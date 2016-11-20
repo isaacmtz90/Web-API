0d28876db29a96dbfc276cdd4ca1e33574b155e5
Fabio Daio <fabio-daio@hotmail.com>
2016-11-20 15:08:38 +0800

    Fix v2 to init.rb  :/

--- services/init.rb
+++ services/init.rb
@@ -2,7 +2,7 @@
 require 'dry-monads'
 require 'dry-container'
 require 'dry-transaction'
-require 'facegroup'
+require 'meetupevents'
 
 Dir.glob("#{File.dirname(__FILE__)}/*.rb").each do |file|
   require file
