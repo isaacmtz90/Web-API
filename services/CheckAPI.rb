0a3a9110b94b70bd0fd432d35618b9cdb6abeec4
Fabio Daio <fabio-daio@hotmail.com>
2016-12-09 14:51:14 +0800

    Refactor some services to use Transactions

--- services/CheckAPI.rb
+++ services/CheckAPI.rb
@@ -3,12 +3,20 @@
 # check given group (url) in the WebAPI
 class CheckAPI
   extend Dry::Monads::Either::Mixin
+  extend Dry::Container::Mixin
 
-  def self.call(meetup_group_url)
+  register :check_api, lambda { |meetup_group_url|
     if Meetup::Group.find(urlname: meetup_group_url)
       Right(Meetup::Group.find(urlname: meetup_group_url))
     else
       Left(Error.new(:bad_request, "Group (url: #{meetup_group_url}) could not be found")) # halt 400
     end
+  }
+
+
+  def self.call(meetup_group_url)
+    Dry.Transaction(container: self) do
+      step :check_api
+    end.call(meetup_group_url)
   end
 end
--- services/CheckDatabase.rb
+++ services/CheckDatabase.rb
@@ -3,12 +3,19 @@
 # check whether group already exists in database
 class CheckDatabase
   extend Dry::Monads::Either::Mixin
+  extend Dry::Container::Mixin
 
-  def self.call(meetup_group_url)
+  register :check_database, lambda { |meetup_group_url|
     if Group.find(urlname: meetup_group_url)
       Left(Error.new(:cannot_process, "Group #{meetup_group_url} already exists")) # halt 422
     else
       Right(Group.find(urlname: meetup_group_url)) # do nothing!
     end
+  }
+
+  def self.call(meetup_group_url)
+    Dry.Transaction(container: self) do
+      step :check_database
+    end.call(meetup_group_url)
   end
 end
